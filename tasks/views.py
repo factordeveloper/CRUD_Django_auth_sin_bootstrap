0fc050d27272da7defdd87addd0838ae497bfaa1
Juan Felipe Herrera Pinzón <77707590+FelipeHerreraPinzon@users.noreply.github.com>
2024-01-15 23:15:13 -0500

    crear tareas

--- tasks/views.py
+++ tasks/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.contrib.auth.models import User
 from django.contrib.auth import login, logout, authenticate
 from django.db import IntegrityError
+from .forms import TaskForm
 
 # Create your views here.
 
@@ -40,6 +41,34 @@ def signup(request):
 def tasks(request):
     return render(request, 'tasks.html')
 
+def create_task(request):
+
+    if request.method == 'GET':
+        return render(request, 'create_task.html', {
+        'form': TaskForm
+    })
+
+    else:
+        try:
+            #print(request.POST)
+            form = TaskForm(request.POST)
+            new_task = form.save(commit=False)
+            new_task.user = request.user
+            new_task.save()
+            #print(new_task)
+            return redirect('tasks')
+        except ValueError:
+            return render(request, 'create_task.html', {
+            'form': TaskForm,
+            'error':'por favor ingresa datos validos'
+        })
+    
+
+
+
+
+
+    
 
 def signout(request):
     logout(request)
